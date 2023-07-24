86b3353071e808e1b7e654c56ec45210ed914096
ronnalddannyel <ronnalddannyel@gmail.com>
2023-07-23 22:56:41 -0400

    up

--- login.php
+++ login.php
@@ -10,7 +10,7 @@ if(empty($_POST['usuario']) || empty($_POST['senha'])) {
 $usuario = mysqli_real_escape_string($conn, $_POST['usuario']);
 $senha = mysqli_real_escape_string($conn, $_POST['senha']);
 
-$query = "select usuario_id, CpfCnpj, NivelDeAcesso from usuario where CpfCnpj= '$usuario' and senha = '$senha' "; 
+$query = "select usuario_id, Cpf from UsuariosInterno where Cpf= '$usuario' and senha = '$senha' "; 
 
 
 
@@ -18,12 +18,11 @@ $result = mysqli_query($conn, $query);
 $row = mysqli_num_rows($result);
 $row2  = mysqli_fetch_assoc($result);
 
-$NivelDeAcesso = $row2['NivelDeAcesso'];
 
 
 if($row == 1) {
 	$_SESSION['usuario'] = $usuario;
-	$_SESSION['NivelDeAcesso'] = $NivelDeAcesso;
+	
 	header('Location: ./PainelAptaInterno/index.php');
 	exit();
 
--- website/index.php
+++ website/index.php
@@ -1,5 +1,5 @@
 <?php $upload_dir = '../Cadastros/Professores/ArquivosEnviados/';?>
-<?php include 'Header.php';?>
+<?php //include 'Header.php';?>
 <?php include 'Menu.php';?>
 
 
